34bada2c89be64566cca2b251c6b3d04a1f6979e
Michelle Cua <ecua@caltech.edu>
2016-09-13 14:34:15 -0700

    Added image files to gitignore

--- e02_fun.py
+++ e02_fun.py
@@ -1,5 +1,7 @@
 # import modules
 import os
+import bioinfo_dicts as bd
+import numpy as np
 
 # E02.1 - read fasta file
 def load_fasta(filename):
@@ -55,6 +57,7 @@ def gc_map(seq, block_size, gc_thresh):
 
     return seq1
 
+# E02.3 - Load in sequence from fasta file, process, and save
 def proc_fasta_gc(fname_in,fname_out,block_size,gc_thresh):
 
     # Load sequence from fasta file
@@ -62,27 +65,182 @@ def proc_fasta_gc(fname_in,fname_out,block_size,gc_thresh):
 
     # Load description from fasta
     with open(fname_in,'r') as f:
-        f_desc = f.read()
+        f_desc = f.readline()
 
     # Process sequence
     seqmap = gc_map(seq,block_size,gc_thresh)
 
     # Add \n every 60 characters
-    nAdd = len(seqmap//60)
+    nAdd    = len(seqmap)//60
+    seqlist = list(seqmap)
 
-    for i in range(nAdd,0,-1):
-        seqmap = seqmap.insert(i*60,'\n')
+    for i in range(0,nAdd+1,1):
+        ind = (nAdd - i)*60
+        seqlist.insert(ind,'\n')
 
+    seqmapOut = ''.join(seqlist)
     # Write Mapped sequences
     with open(fname_out,'w') as f:
         f.write(f_desc)
-        f.write(seqmap)
+        f.write(seqmapOut)
 
-    return 0
+    # Print and add every 60 characters
+    # with open(fname_out,'w') as f2:
+    #     f2.write(f_desc)
+    #     for i in range(0,len(seqmap),60):
+    #         f2.write(seqmap[i:i+60] + ['\n'])
 
+    return seqmapOut
+
+# E02.4a Detect ORF sequence
 def orf_detect(seq):
     """Detects ORF sequences in DNA sequences"""
-    ORF_start = 'ATG'
-    ORG_end   = ['TGA','TAG','TAA']
+    ORF_start = ['ATG']
+    ORF_ends  = ['TGA','TAG','TAA']
 
-    
+    # Find all indices corresponding to start and end sequences
+    stInds    = subseq_detect(seq,ORF_start)
+    endInds   = subseq_detect(seq,ORF_ends)
+
+    print('Detected ',len(stInds),' instances of start seq')
+    print('Detected ',sum(endInds), ' instances of end seq')
+
+    endInds   = endInds[::-1]
+    indDiff   = []
+    maxDiff   = 0
+
+    for i in range(0,len(stInds)):
+        for j in range(0,len(endInds)):
+            diff = endInds[j]-stInds[i]
+            # If start and end pair form possible ORF sequence, then record
+            if diff%3 == 0 and diff > maxDiff:
+                maxDiff = max(diff,maxDiff)
+                indDiff = [stInds[i],endInds[j]]
+
+    print('Longest Detected Sequences is',maxDiff+3,' long')
+    return seq[indDiff[0]:(indDiff[1]+3)]
+
+# E02.4b Convert DNA to Protein sequence
+def dna_to_protein(seq,fname=''):
+    """Converts DNA sequence to Protein"""
+    proteinList = ''
+
+    for i in range(0,len(seq),3):
+        proteinList += bd.codons[seq[i:i+3]]
+
+    # if specified, write protein into specified file
+    if fname != '':
+        with out(fname) as f:
+            f.write(proteinList)
+
+    return proteinList;
+
+# E02.4e Detect ORF sequence -multiple
+def orf_detect_multi(seq,n=1):
+    """Detects ORF sequences in DNA sequences"""
+    ORF_start = ['ATG']
+    ORF_ends  = ['TGA','TAG','TAA']
+
+    # Find all indices corresponding to start and end sequences
+    stInds    = subseq_detect(seq,ORF_start)
+    endInds   = subseq_detect(seq,ORF_ends)
+
+    print('Detected ',len(stInds),' instances of start seq')
+    print('Detected ',sum(endInds), ' instances of end seq')
+
+    indDiffs = []
+    numDiffs = []
+
+    for i in range(0,len(stInds)):
+        for j in range(0,len(endInds)):
+            diff = endInds[j]-stInds[i]
+            # If start and end pair form possible ORF sequence, then record
+            if diff%3 == 0:
+                numDiffs.append(diff)
+                indDiffs.append([stInds[i], endInds[j]])
+
+    # get indices to n largest diffs
+    indOrd    = np.argsort(numDiffs)
+    lenOrd    = len(indOrd)
+    indOrdMax = indOrd[lenOrd-n:lenOrd]
+    ords      = []
+    maxDiff   = []
+
+    for ind in indOrdMax:
+        ords.append(seq[indDiffs[ind][0]:indDiffs[ind][1]+3])
+        maxDiff.append(numDiffs[ind]+3)
+
+    print(n,' largest ords are lengths ', maxDiff)
+    return ords
+
+# E02.4e - v2 Detect multiple orfs - no numpy
+# E02.4e Detect ORF sequence -multiple
+def orf_detect_multi_b(seq,n=1):
+    """Detects ORF sequences in DNA sequences"""
+    ORF_start = ['ATG']
+    ORF_ends  = ['TGA','TAG','TAA']
+
+    # Find all indices corresponding to start and end sequences
+    stInds    = subseq_detect(seq,ORF_start)
+    endInds   = subseq_detect(seq,ORF_ends)
+
+    print('Detected ',len(stInds),' instances of start seq')
+    print('Detected ',sum(endInds), ' instances of end seq')
+
+    indDiffs = []
+    numDiffs = []
+    numDiffsA = []
+
+    for i in range(0,len(stInds)):
+        for j in range(0,len(endInds)):
+            diff = endInds[j]-stInds[i]
+            # If start and end pair form possible ORF sequence, then record
+            if diff%3 == 0:
+                numDiffs.append(diff)
+                numDiffsA.append(diff)
+                indDiffs.append([stInds[i], endInds[j]])
+
+    # get indices to n largest diffs
+    numDiffs.sort()
+    lenOrd  = len(numDiffs)
+    maxDiff = numDiffs[lenOrd-n:lenOrd]
+    ords    = []
+
+    print(n,' largest ords are lengths (-3) is', maxDiff)
+
+    for ind,dif in enumerate(numDiffsA):
+        if ind in maxDiff:
+            ords.append(seq[indDiffs[ind][0]:indDiffs[ind][1]+3])
+        if len(ords) == n:
+            break
+
+    return ords
+
+# E02.4a - i Detect codons in sequence
+def subseq_detect(seq,codon):
+    """Detects 3 letter codon in seq"""
+
+    # Count the number of codons
+    numCount = []
+    for i in range(0,len(codon)):
+        numCount.append(seq.count(codon[i]))
+
+    # print(numCount)
+    # Find all indices
+    indices = []
+    for i in range(0,len(numCount)):
+        j  = 0
+        nC = 0
+        while j < len(seq)-3 and nC < numCount[i]:
+            tmp = seq[j::].find(codon[i])
+            if tmp == -1 : # no more left
+                j = len(seq)
+                break;
+            else: #found sequence - add to list
+                indices.append(tmp+j)
+                j = tmp + j + 3
+                nC += 1
+
+    indices.sort()
+
+    return indices
--- lesson20.py
+++ lesson20.py
@@ -1,16 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.special
+
+rc={'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
+sns.set(rc=rc)
 
 # Test Data
 xa_high = np.loadtxt('data/xa_high_food.csv')
 xa_low  = np.loadtxt('data/xa_low_food.csv')
 
-# Array Generation
-A = np.array([[6.7, 1.3, 0.6, 0.7],
-              [0.1, 5.5, 0.4, 2.4],
-              [1.1, 0.8, 4.5, 1.7],
-              [0.0, 1.5, 3.4, 7.5]])
+x      = np.linspace(-15,15,400)
+norm_I = 4*(scipy.special.j1(x)/x)**2
 
-b = np.array([1.1, 2.3, 3.3, 3.9])
+# plt.close()
+# plt.plot(x,norm_I,marker='.',linestyle='none')
+# plt.xlabel('$x$')
+# plt.ylabel('$I(x)/I_0$')
+# plt.margins(0.02)
+# plt.show()
+
+# Array Generation
+# A = np.array([[6.7, 1.3, 0.6, 0.7],
+#               [0.1, 5.5, 0.4, 2.4],
+#               [1.1, 0.8, 4.5, 1.7],
+#               [0.0, 1.5, 3.4, 7.5]])
+#
+# b = np.array( [1.1, 2.3, 3.3, 3.9])
+
+# Processing Spike Data
+data = np.loadtxt('data/retina_spikes.csv',delimiter=',',skiprows=2)
+t    = data[:,0]
+volt = data[:,1]
+
+plt.close()
+plt.plot(t,volt);
+plt.xlabel('Time (ms)')
+plt.ylabel('Voltage (µV)')
+plt.xlim(1395,1400)
+plt.show()
 
 # Functions
 def xa_to_diameter(xa):
@@ -23,3 +51,26 @@ def xa_to_diameter(xa):
     diameter = 2*np.sqrt(xa/np.pi)
 
     return diameter
+
+def easy_reshape(arr,ncols,order='C'):
+    """
+    Guarantee that reshaping works by defining only one shape parameter.
+    """
+    outArr = np.reshape(arr,[ncols,len(arr)/ncols],order)
+    return outArr
+
+def plotwrap_xa():
+    low_min  = np.min(xa_low)
+    low_max  = np.max(xa_low)
+    high_min = np.min(xa_high)
+    high_max = np.max(xa_high)
+    glob_min = ( np.min([low_min,high_min])//50)*50
+    glob_max = ( np.max([low_max,high_max])//50)*50
+    bins = np.arange(glob_min-50,glob_max+100,50)
+    _ = plt.hist(xa_low , normed=True, bins=bins)
+    _ = plt.hist(xa_high, normed=True, bins=bins, alpha=0.5)
+    plt.xlabel('Cross-sectional area (µm$^2$)')
+    plt.ylabel('Frequency')
+    plt.legend(('Low','High'))
+    plt.savefig('tempfig.pdf',bbox_inches ='tight')
+    plt.show()
