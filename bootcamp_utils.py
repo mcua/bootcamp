a0065047d4400c5b70c2368bc9f65c39207a2af8
Michelle Cua <ecua@caltech.edu>
2016-09-14 11:55:53 -0700

    Added Bootstrap method to bootcamp_utils.py

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -1,4 +1,7 @@
-# bootcamp_utils: a collection of statistical functions
+"""
+bootcamp_utils: a collection of statistical functions
+"""
+import numpy as np
 
 def ecdf(data):
     """
@@ -8,3 +11,12 @@ def ecdf(data):
     y = np.arange(1,len(x)+1)/len(x)
 
     return x,y
+
+def draw_bs_reps(data, func=np.mean, sz = 100):
+    reps = np.empty([sz,1])
+    for i in range(0,sz):
+        bs_samp = np.random.choice(data,size=len(data))
+        reps[i] = func(bs_samp)
+
+    conf_int = np.percentile(reps,[2.5,97.5])
+    return reps,conf_int
--- lesson23.py
+++ lesson23.py
@@ -15,15 +15,23 @@ iptg = data_txt[:,0]
 gfp  = data_txt[:,1]
 sem  = data_txt[:,2]
 
-# plt.semilogx(iptg,gfp,linestyle = 'none',marker='.',markersize=20)
-# plt.xlabel('IPTG (mM)')
-# plt.ylabel('Normalized GFP')
+x = np.linspace(1600,2500,400)
+cdf_high = scipy.stats.norm.cdf(x,loc=np.mean(xa_high),
+                                  scale=np.std(xa_high))
+cdf_low  = scipy.stats.norm.cdf(x,loc=np.mean(xa_low),
+                                  scale=np.std(xa_low))
 
-# plt.errorbar(iptg,gfp,yerr=sem,linestyle='none',marker='.',markersize=10)
-# plt.xscale('log')
-# plt.xlabel('IPTG (mM)')
-# plt.ylabel('Normalized GFP')
-# plt.show()
+plt.figure(1)
+plt.semilogx(iptg,gfp,linestyle = 'none',marker='.',markersize=20)
+plt.xlabel('IPTG (mM)')
+plt.ylabel('Normalized GFP')
+
+plt.figure(2)
+plt.errorbar(iptg,gfp,yerr=sem,linestyle='none',marker='.',markersize=10)
+plt.xscale('log')
+plt.xlabel('IPTG (mM)')
+plt.ylabel('Normalized GFP')
+plt.show()
 
 # Functions
 def ecdf(data):
@@ -36,9 +44,11 @@ def ecdf(data):
 def plotwrp_ecdf():
     ecdf_high = ecdf(xa_high)
     ecdf_low  = ecdf(xa_low)
-
-    plt.plot(ecdf_high[:,0],ecdf_high[:,1],marker='.',linestyle ='none',alpha=0.5,markersize=10)
-    plt.plot(ecdf_low[:,0],ecdf_low[:,1],marker='.',linestyle ='none',alpha=0.5,markersize=10)
+    plt.figure(1)
+    plt.plot(ecdf_high[:,0],ecdf_high[:,1],marker='.',linestyle ='none',alpha=0.5,markersize=20)
+    plt.plot(ecdf_low[:,0] ,ecdf_low[:,1] ,marker='.',linestyle ='none',alpha=0.5,markersize=20)
+    plt.plot(x,cdf_high,color='gray')
+    plt.plot(x,cdf_low,color='gray')
     plt.ylabel('ECDF')
     plt.xlabel('Cross Sectional Area ($\mu$m)')
     plt.legend(('high','low'),loc='best',fontsize=14)
